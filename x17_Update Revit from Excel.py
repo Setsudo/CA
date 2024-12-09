c144f50534989abe19018a431d4b74d16829d137
Michael McCune <michael.c.mccune@gmail.com>
2024-12-09 17:13:40 -0600

    Debug Added to Each Sub-Header

--- x17_Update Revit from Excel.py
+++ x17_Update Revit from Excel.py
@@ -3,14 +3,13 @@ revit_data = IN[1]
 
 def normalize_subheader_name(name):
     # Convert name to string, replace newlines with a space, and normalize spaces
-    return " ".join(str(name).replace("\n", " ").split())
+    return " ".join(str(name).replace("\n", " ").replace("\r", " ").split())
 
 def extract_subheaders(data):
     subheader_names = []
-    if len(data) > 0 and isinstance(data[0], list):
-        # data[0] should be a list of sub-header blocks
-        subheader_blocks = data[0]
-        for block in subheader_blocks:
+    if isinstance(data, list):
+        # Iterate through the blocks directly
+        for block in data:
             # Each block should have something like ["Sub-Header", "<Name>", ...]
             if isinstance(block, list) and len(block) > 1:
                 # Check if block[0] indicates a sub-header
@@ -24,5 +23,13 @@ def extract_subheaders(data):
 excel_subheaders = extract_subheaders(excel_data)
 revit_subheaders = extract_subheaders(revit_data)
 
-# Return results for inspection
-OUT = (excel_subheaders, revit_subheaders)
+# Add debug notes directly within the Sub-Header block
+for block in revit_data:
+    if isinstance(block, list) and len(block) > 1 and "Sub-Header" in str(block[0]):
+        raw_name = str(block[1])
+        subheader_name = normalize_subheader_name(raw_name)
+        if subheader_name in excel_subheaders:
+            block.insert(2, "Debug Note: Subheader Matched in Excel")
+
+# Return the modified Revit data
+OUT = revit_data
